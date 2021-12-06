68594031847afd64b42da4f431c8303c5f82f8f6
LloydTao <LL23@live.co.uk>
2021-12-06 19:58:04 +0000

    Add main file boilerplate

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def simulate(generations=255):
+    """Run the main loop of the evolutionary algorithm.
+
+    Args:
+        generations (int, optional): Number of iterations. Defaults to 255.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
